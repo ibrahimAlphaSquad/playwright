f3ac4b860302a0e5ae2ece320dbaeb5749ac795f
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2024-02-26 23:17:39 +0500

    login test updated

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,7 +1,12 @@
 import { test } from "@playwright/test";
 
-test("should navigate to the about page", async ({ page }) => {
+test("should navigate to monkeytilt", async ({ page }) => {
     await page.goto('https://staging.monkeytilt.com/');
+
+    await page.locator('input[name="_vercel_password"]').fill("xB5vXCFtlrdpVsk");
+
+    await page.locator('button:has-text("Log in")').click();
+
     // Click on Login button and fill the form
     await page.click('a[href="/login"]');
     // Click on Username and fill up the field

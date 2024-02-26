647f13f0baf3292ad25e82eadee9383f5c614a49
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2024-02-27 00:35:37 +0500

    playwright config updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
-import { defineConfig, devices } from "@playwright/test";
-import path from "path";
+import { defineConfig, devices } from '@playwright/test';
 
 // Use process.env.PORT by default and fallback to port 3000
 const PORT = process.env.PORT || 3000;
@@ -7,13 +6,8 @@ const PORT = process.env.PORT || 3000;
 // Set webServer.url and use.baseURL with the location of the WebServer respecting the correct set port
 const baseURL = `http://localhost:${PORT}`;
 
-// Reference: https://playwright.dev/docs/test-configuration
 export default defineConfig({
-  // Timeout per test
-  timeout: 60 * 1000,
-  // Test directory
-  testDir: path.join(__dirname, "e2e"),
-  // If a test fails, retry it additional 2 times
+  testDir: 'tests',
 
   // Run all tests in parallel.
   fullyParallel: false,
@@ -21,16 +15,14 @@ export default defineConfig({
   // Retry on CI only.
   retries: 2,
 
-
   // Opt out of parallel tests on CI.
   workers: 1,
 
   // Reporter to use
-  reporter: 'html',
+  reporter: 'list',
 
+  timeout: 60000,
 
-  // Run your local dev server before starting the tests:
-  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
     command: "npm run dev",
     url: baseURL,
@@ -39,49 +31,34 @@ export default defineConfig({
   },
 
   use: {
-    // Use baseURL so to make navigations relative.
-    // More information: https://playwright.dev/docs/api/class-testoptions#test-options-base-url
     baseURL,
-
-    // Retry a test if its failing with enabled tracing. This allows you to analyze the DOM, console logs, network traffic etc.
-    // More information: https://playwright.dev/docs/trace-viewer
     trace: "retry-with-trace",
-
-    // All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
-    // contextOptions: {
-    //   ignoreHTTPSErrors: true,
-    // },
   },
-
+  /* Configure projects for major browsers */
   projects: [
     {
-      name: "Desktop Chrome",
-      use: {
-        ...devices["Desktop Chrome"],
-      },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], viewport: { width: 1920, height: 1080 }, },
     },
+
     // {
-    //   name: 'Desktop Firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //   },
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
     // },
     // {
-    //   name: 'Desktop Safari',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //   },
-    // },
-    // Test against mobile viewports.
-    // {
-    //   name: "Mobile Chrome",
-    //   use: {
-    //     ...devices["Pixel 5"],
-    //   },
-    // },
-    // {
-    //   name: "Mobile Safari",
-    //   use: devices["iPhone 12"],
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
     // },
   ],
 });

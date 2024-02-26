46183e6c6f9cda57662418083004d62949ba6b4b
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2024-02-26 22:58:56 +0500

    test update

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,4 +1,6 @@
-module.exports = async ({ page }) => {
+import { test } from "@playwright/test";
+
+test("should navigate to the about page", async ({ page }) => {
     await page.goto('https://staging.monkeytilt.com/');
     // Click on Login button and fill the form
     await page.click('a[href="/login"]');
@@ -10,4 +12,4 @@ module.exports = async ({ page }) => {
     await page.locator('//input[@placeholder="Password"]').fill(process.env.PASSWORD);
     // Click on Login button and wait to load Home page
     await page.getByRole('button', { name: 'LOGIN' }).click();
-};
+});

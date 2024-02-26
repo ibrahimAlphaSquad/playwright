3cd249395c75220de7030d38f32e8dbbb5f6b041
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2024-02-26 23:46:39 +0500

    login test updated

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -8,17 +8,16 @@ test("should navigate to monkeytilt", async ({ page }) => {
     // Navigate to the monkeytilt website
     await page.goto('https://staging.monkeytilt.com/');
 
-    await page.locator('input[name="_vercel_password"]').fill("xB5vXCFtlrdpVsk");
+    // xB5vXCFtlrdpVsk
+    await page.locator('input[name="_vercel_password"]').fill("fail test");
 
     await page.locator('button:has-text("Log in")').click();
 
     // Click on Login button and fill the form
     await page.click('a[href="/login"]');
     // Click on Username and fill up the field
-    // USERNAME=arslankhan
     await page.getByPlaceholder('Username').fill(process.env.USERNAME);
     // Click on Password and fill up the field
-    // PASSWORD=test1234
     await page.locator('//input[@placeholder="Password"]').fill(process.env.PASSWORD);
     // Click on Login button and wait to load Home page
     await page.getByRole('button', { name: 'LOGIN' }).click();

f6fef0e16fd48cc939dda67212b5df32edaf8b71
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2024-03-04 23:16:54 +0500

    light house 9
    light house config

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,19 +4,19 @@ module.exports = {
         upload: {
             target: 'temporary-public-storage',
         },
-        collect: {
-            staticDistDir: '.next/',
-            url: ['http://localhost:3000'],
-            startServerCommand: 'npm run start',
-        },
-        assert: {
-            preset: 'lighthouse:no-pwa',
-            assertions: {
-                'categories:performance': ['error', {minScore: 0.9}],
-                'categories:accessibility': ['error', {minScore: 0.9}],
-                'categories:best-practices': ['error', {minScore: 0.9}],
-                'categories:seo': ['error', {minScore: 0.9}],
-            },
-        }
+        // collect: {
+        //     staticDistDir: '.next/',
+        //     url: ['http://localhost:3000'],
+        //     startServerCommand: 'npm run start',
+        // },
+        // assert: {
+        //     preset: 'lighthouse:no-pwa',
+        //     assertions: {
+        //         'categories:performance': ['error', {minScore: 0.9}],
+        //         'categories:accessibility': ['error', {minScore: 0.9}],
+        //         'categories:best-practices': ['error', {minScore: 0.9}],
+        //         'categories:seo': ['error', {minScore: 0.9}],
+        //     },
+        // }
     },
 };

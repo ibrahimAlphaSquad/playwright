203df06854bccdefbb1db3dd3f7b86eebc28a446
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2024-03-04 23:09:11 +0500

    lighthouserc config added

---
+++ lighthouserc.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+    ci: {
+        upload: {
+            target: 'temporary-public-storage',
+        },
+        collect: {
+            staticDistDir: '.next/',
+            url: ['http://localhost:3000'],
+            startServerCommand: 'npm run start',
+        },
+        assert: {
+            preset: 'lighthouse:no-pwa',
+            assertions: {
+                'categories:performance': ['error', {minScore: 0.9}],
+                'categories:accessibility': ['error', {minScore: 0.9}],
+                'categories:best-practices': ['error', {minScore: 0.9}],
+                'categories:seo': ['error', {minScore: 0.9}],
+            },
+        }
+    },
+};

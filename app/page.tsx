1da653482ae3b4e8f28f26ef5082fa8980177b66
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2024-03-25 23:18:13 +0500

    push to UI branch

--- app/page.tsx
+++ app/page.tsx
@@ -25,6 +25,7 @@ export default function Page() {
       <p>This sentence is designed to test the deployment of "merge-test" when a pull request (PR) opened with "merge-test" is merged. Upon merging the PR, it triggers the deployment; otherwise, no deployments should execute.</p>
       <p>Test 3</p>
       <p>UI Branch should not deploy</p>
+      <p>This is direct push to UI and it should not deploy</p>
       <form onSubmit={submitHandler} method="post">
         <label>
           Email:

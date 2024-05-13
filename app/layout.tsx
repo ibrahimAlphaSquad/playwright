41ff9262f6f4d93de9c723f6721b982ec720758d
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2024-05-13 21:08:51 +0500

    requestmetrics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,33 @@
+import Script from "next/script";
+
 export const metadata = {
   title: {
-    default: 'Basic testing arena',
+    default: "Basic testing arena",
   },
-  description: 'A basic testing arena for Next.js',
+  description: "A basic testing arena for Next.js",
   twitter: {
-    card: 'summary_large_image'
+    card: "summary_large_image",
   },
   openGraph: {
-    title: 'Next.js',
-    description: 'The React Framework for the Web',
-    url: 'https://nextjs.org',
-    siteName: 'Next.js',
+    title: "Next.js",
+    description: "The React Framework for the Web",
+    url: "https://nextjs.org",
+    siteName: "Next.js",
     images: [
       {
-        url: 'https://nextjs.org/og.png', // Must be an absolute URL
+        url: "https://nextjs.org/og.png", // Must be an absolute URL
         width: 800,
         height: 600,
       },
       {
-        url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
+        url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
         width: 1800,
         height: 1600,
-        alt: 'My custom alt',
+        alt: "My custom alt",
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
   },
 };
 
@@ -34,11 +36,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
-
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          src="https://cdn.requestmetrics.com/agent/current/rm.js"
+          strategy="afterInteractive"
+          data-rm-token="r3ax6ba:p8yi8qd"
+        />
+      </body>
     </html>
   );
 }
